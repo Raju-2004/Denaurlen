1786d85c60b491acd920342f57d7a8af665070bc
kusa raju <21pa1a05e9@vishnu.edu.in>
2024-04-09 11:45:38 +0530

    add cors

--- server/index.js
+++ server/index.js
@@ -8,10 +8,12 @@ const { mongoDbUrL, PORT } = require("./config/configuration");
 const userRoutes = require('./routes/user')
 const otpRoutes = require('./routes/Otp')
 
-app.use(cors({
-    origin: 'https://denaurlen-brown.vercel.app/',
-    credentials: true,
-  }));
+// app.use(cors({
+//     origin: 'https://denaurlen-brown.vercel.app/',
+//     credentials: true,
+//   }));
+
+app.use(cors())
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
